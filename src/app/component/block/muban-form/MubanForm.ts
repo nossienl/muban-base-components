bdf1ece9ebbb9c62fc331f829c7c66d54019692f
Niels Otterspoor <niels.otterspoor@gmail.com>
2019-04-04 10:36:25 +0200

    Removed fields and combine in 1 Add checkbox

--- src/app/component/block/muban-form/MubanForm.ts
+++ src/app/component/block/muban-form/MubanForm.ts
@@ -1,4 +1,5 @@
 import AbstractBlock from '../AbstractBlock';
+import validate from 'validate.js';
 
 export default class MubanForm extends AbstractBlock {
   static displayName: string = 'muban-form';
